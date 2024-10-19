9c369c94e2e2e1dc89f2e713c696613273fbd9d3
Daniel <dperezterron19@gmail.com>
2024-10-19 20:05:37 +0200

    Cambios+InputController

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
