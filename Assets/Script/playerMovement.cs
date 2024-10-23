83b34ac8cd2b1aa6e9b0bf77de6fe4da10826f97
Dana69UNI <dperezterron19@gmail.com>
2024-10-23 09:13:28 +0200

    Cinemachine + PlayerNormalize movement

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -3,42 +3,55 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public Camera playerCamera;
     public float moveSpeed = 5.0f; //Este sera el multiplicador del input del movimiento
-    public float walkingSpeed = 5.0f; //Referencia para modificar moveSpeed, así no ponemos un numero arbitrario, sino que ya esta declarado
-    public float sprintSpeed = 10.0f; //Referencia para modificar moveSpeed, pa correr
+    public float walkingSpeed = 5.0f;  //Referencia para modificar moveSpeed, así no ponemos un numero arbitrario, sino que ya esta declarado
+    public float sprintSpeed = 10.0f;//Referencia para modificar moveSpeed, pa correr
     public InputActionReference move;
-    PlayerInputActions inputActions;
+
     private Vector2 _moveDirection;
-    
 
     private void Update()
     {
         _moveDirection = move.action.ReadValue<Vector2>();
-        
     }
+
     private void FixedUpdate()
     {
         OnMove();
     }
+
     private void OnMove()
     {
-        rb.velocity = new Vector3(x: _moveDirection.x * moveSpeed, y: rb.velocity.y, z: _moveDirection.y * moveSpeed);
+     
+        Vector3 forward = playerCamera.transform.forward;
+        Vector3 right = playerCamera.transform.right;
+
+        
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
        
+        Vector3 moveDirection = (right * _moveDirection.x + forward * _moveDirection.y).normalized;
+
+       
+        rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, moveDirection.z * moveSpeed);
     }
+
     public void OnSprintStart() //Al haber separado Sprint en Start y end, podemos usar tal cual el OnSprintxxx de funcion y no tener que hacer booleanas ni nada del estilo
     {
         moveSpeed = sprintSpeed;
     }
+
     public void OnSprintEnd()
     {
         moveSpeed = walkingSpeed;
     }
-
-
 }
-               
